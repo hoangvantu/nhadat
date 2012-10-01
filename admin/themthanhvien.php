7be896ae44b1ce9e40e140ab20ffc0d356502fe6
adasd <asdasdas>
2012-10-01 18:35:21 +0700

    9999
    9999

---
+++ admin/themthanhvien.php
@@ -0,0 +1,81 @@
+<?php
+
+include_once('../connect.php');
+require_once '../includes/sm_config_admin.php';
+$db = new dbmanager();
+$db->connect();
+$thongbaoloi = '';
+
+   
+if(isset($_POST['btnsubmit']))
+{
+    
+    if(isset($_POST['txtemail']))
+    {
+        if($_POST['txtemail']="")
+        {
+        $thongbaoloi=$thongbaoloi."Bạn chưa điền email</br>";
+      
+        }
+        else  if(!(filter_var($_POST['txtemail'], FILTER_VALIDATE_EMAIL)))
+         {
+           $thongbaoloi =$thongbaoloi. '<br/> Email bạn điền không đúng định dạng</br>';
+        }
+        else $email = $_POST['txtemail'];
+    }
+    else 
+    {
+          
+        $thongbaoloi =$thongbaoloi."Ban la hacker a.</br>";
+    }
+    
+    if(isset($_POST['txtname']))
+    {
+        if($_POST['txtname']=="")
+        {
+            $thongbaoloi="Ban phai khai bao ten cua ban</br>";
+           
+        }
+        $name = $_POST['txtname'];
+    }
+      else $thongbaoloi =$thongbaoloi."Ban la hacker a.</br>";
+      if(isset($_POST['txtpass']))
+      {
+          if($_POST['txtpass']=="")
+          {
+              $thongbaoloi=$thongbaoloi."ban phai dien may khau</br>";
+          }
+          else if(isset($_POST['txtpass2']))
+          {
+              if($_POST['txtpass']==$_POST['txtpass2'])
+              {
+                  if(strlen($_POST['txtpass'])<=6)
+                  {
+                      $thongbaoloi=$thongbaoloi."Mat khau cua ban it nhat la 6 ky tu </br>";
+                  }
+                  else $pass=$_POST['txtpass'];
+              }
+              else $thongbaoloi=$thongbaoloi."Mat khau khong trung nhau</br>";
+          }
+          else $thongbaoloi =$thongbaoloi."Ban la hacker a.</br>";
+          
+      }
+      else $thongbaoloi = $thongbaoloi."Ban la hacker a.</br>";
+   if($thongbaoloi!="")
+   {
+       $qlgiaodien->assign('thongbaoloi',$thongbaoloi);
+   }
+   else      
+   {
+     
+   }
+    
+}
+
+
+    
+$giaodien = 'themthanhvien.tpl';
+$qlgiaodien->assign('page_title', "them thanh vien");
+
+$qlgiaodien->display($giaodien);
+?>
